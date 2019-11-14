774c832a09475db075af41f3acbcfba0466abc3b
mshumko <msshumko@gmail.com>
2019-11-14 09:00:24 -0700

    Lots of changes.
    Added png files to the .gitignore. Organized the repo and
    added the plot_curtain_examples to the fig_scripts folder.
    
    Modified the plot_curtains to plot curtains from the microburst
    dataset, after spatial correlation filtering.
    
    Also copied over the microburst_browser_v2 from the AC6
    microburst size dist project. I will need to make a few modifications
    to this program.

---
+++ stats/microburst_browser_2.py
@@ -0,0 +1,245 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+import pandas as pd
+from datetime import date, datetime
+from ac6_microburst_scale_sizes.validation.plot_microbursts import PlotMicrobursts
+from matplotlib.widgets import Button, TextBox
+
+catalog_save_dir = ('/home/mike/research/ac6_microburst_scale_sizes/data/'
+                    'coincident_microbursts_catalogues')
+
+class Browser(PlotMicrobursts):
+    def __init__(self, catalog_version, plot_width=5, 
+                catalog_save_name=None, width_tol=0.1, filterDict={}, 
+                jump_to_latest=True):
+        """
+        This class plots the AC6 microbursts and allows the user to browse
+        detections in the future and past with buttons. Also there is a button
+        to mark the event as a microburst.
+        """
+        PlotMicrobursts.__init__(self, catalog_version, plot_width=plot_width, 
+                                plot_width_flag=False, make_plt_dir_flag=False)
+        self.filter_catalog(filterDict=filterDict)
+        # Filter out events with widths whithin a width_tol.
+        if width_tol is not None:
+            self.catalog = self.catalog[np.isclose(
+                            self.catalog['peak_width_A'], 
+                            self.catalog['peak_width_B'], rtol=width_tol)]
+
+        if catalog_save_name is None:
+            self.catalog_save_name = ('AC6_coincident_microbursts_sorted_'
+                                'v{}.txt'.format(catalog_version))
+        else:
+            self.catalog_save_name = catalog_save_name
+        self.catalog_save_path = os.path.join(catalog_save_dir, 
+                                            self.catalog_save_name)
+
+        # Load the filtered catalog if it already exists. This is
+        # userful if you can't sort all microbursts at once!
+        if os.path.exists(self.catalog_save_path):
+            self.load_filtered_catalog()
+        else:
+            self.microburst_idx = np.array([])
+
+        self.current_date = date.min
+        self._init_plot()
+        if jump_to_latest and len(self.microburst_idx):
+            self.index = self.microburst_idx[-1]
+        else:
+            # Start at row 0 in the dataframe.
+            self.index = 0 
+        self.plot()
+        return
+
+    def next(self, event):
+        """ Plots the next detection """
+        # Just return if at the end of the dataframe.
+        if self.index + 1 >= self.catalog.shape[0]:
+            return
+        self.index += 1
+        self.plot()
+        return
+
+    def prev(self, event):
+        """ Plots the previous detection """
+        # Just return if at the end of the dataframe.
+        if self.index == 0:
+            return
+        self.index -= 1
+        self.plot()
+        return
+
+    def append_remove_microburst(self, event):
+        """ 
+        Appends or removes the current catalog row to 
+        self.filtered_catalog which will then
+        be saved to a file for later processing.
+        """
+        if self.index not in self.microburst_idx:
+            self.microburst_idx = np.append(self.microburst_idx, self.index)
+            self.bmicroburst.color = 'g'
+            print('Micorburst saved at', self.catalog.iloc[self.index].dateTime)
+        else:
+            self.microburst_idx = np.delete(self.microburst_idx, 
+                np.where(self.microburst_idx == self.index)[0])
+            self.bmicroburst.color = '0.85'
+            print('Micorburst removed at', self.catalog.iloc[self.index].dateTime)
+        return
+
+    def key_press(self, event):
+        """
+        Calls an appropriate method depending on what key was pressed.
+        """
+        if event.key == 'm':
+            # Mark as a microburst
+            self.append_remove_microburst(event)
+        elif event.key == 'a':
+            # Move self.index back and replot.
+            self.prev(event)
+        elif event.key =='d':
+            # Move the self.index forward and replot.
+            self.next(event)
+        return
+       
+    def change_index(self, index):
+        self.index = int(index)
+        self.plot()
+        return
+
+    def plot(self):
+        """ 
+        Given a self.current_row in the dataframe, make a space-time plot 
+        """
+        print('Index position = {}/{}'.format(
+                    self.index, self.catalog.shape[0]-1))
+        current_row = self.catalog.iloc[self.index]
+        self.index_box.set_val(self.index)
+        self._clear_ax()
+
+        if current_row['dateTime'].date() != self.current_date:
+            # Load current day AC-6 data if not loaded already
+            print('Loading data from {}...'.format(current_row['dateTime'].date()), 
+                    end=' ', flush=True)
+            self.load_ten_hz_data(current_row.dateTime.date())
+            self.current_date = current_row.dateTime.date()
+            print('done.')
+
+        # Turn microburst button green if this index has been marked as a microburst.
+        if self.index in self.microburst_idx:
+            self.bmicroburst.color = 'g'
+        else:
+            self.bmicroburst.color = '0.85'
+           
+        self.make_plot(current_row, savefig=False)
+        self.ax[0].set_title('AC6 Microburst Browser\n {} {}'.format(
+                        current_row['dateTime'].date(), 
+                        current_row['dateTime'].time()))
+        self.ax[0].set_ylabel('mean-subtracted dos1rate\n[counts/s]')
+        self.ax[1].set_ylabel('mean-subtracted dos1rate\n[counts/s]')
+        self.ax[1].set_xlabel('UTC')
+        
+        self._print_aux_info(current_row)
+        plt.draw()
+        return
+
+    def _print_aux_info(self, current_row):
+        """ Print separation info as well as peak width info to the canvas. """
+        self.textbox.clear()
+        self.textbox.axis('off')
+        col1 = ('Lag_In_Track = {} s\nDist_In_Track = {} km\n'
+                    'Dist_total = {} km\npeak_width_A = {} s\n'
+                    'peak_width_B = {} s'.format(
+                    round(current_row['Lag_In_Track'], 1), 
+                    round(current_row['Dist_In_Track'], 1), 
+                    round(current_row['Dist_Total'], 1), 
+                    round(current_row['peak_width_A'], 2), 
+                    round(current_row['peak_width_B'], 2)))
+        col2 = ('time_cc = {}\nspace_cc = {}\n'.format(
+                    round(current_row['time_cc'], 2), 
+                    round(current_row['space_cc'], 1)
+                    ))
+        self.textbox.text(0, 1, col1, va='top')
+        self.textbox.text(1.3, 1, col2, va='top')
+        return
+
+    def _clear_ax(self):
+        [a.clear() for a in self.ax]
+        return 
+
+    def _init_plot(self):
+        """
+        Initialize subplot objects and text box.
+        """
+        fig, self.ax = plt.subplots(2, figsize=(8, 7))
+        plt.subplots_adjust(bottom=0.2)
+
+        # Define button axes.
+        self.axprev = plt.axes([0.54, 0.06, 0.12, 0.075])
+        self.axburst = plt.axes([0.67, 0.06, 0.13, 0.075])
+        self.axnext = plt.axes([0.81, 0.06, 0.12, 0.075])
+
+        # Define buttons and their actions.
+        self.bnext = Button(self.axnext, 'Next (d)', hovercolor='g')
+        self.bnext.on_clicked(self.next)
+        self.bprev = Button(self.axprev, 'Previous (a)', hovercolor='g')
+        self.bprev.on_clicked(self.prev)
+        self.bmicroburst = Button(self.axburst, 'Microburst (m)', hovercolor='g')
+        self.bmicroburst.on_clicked(self.append_remove_microburst)
+
+        # Define the textbox axes.
+        self.textbox = plt.axes([0.1, 0.05, 0.2, 0.075])
+        self.textbox.axis('off')
+        # Define index box.
+        self.axIdx = plt.axes([0.59, 0.01, 0.32, 0.04])
+        self.index_box = TextBox(self.axIdx, 'Index')
+        self.index_box.on_submit(self.change_index)
+
+        # Initialise button press
+        fig.canvas.mpl_connect('key_press_event', self.key_press)
+        return
+
+    def save_filtered_catalog(self):
+        """
+        For every index that a user clicked microburst on, save
+        those rows from the catalog into a new catalog with the
+        name of self.catalog_save_name.
+        """
+        # Return if there are no micriobursts to save.
+        if not hasattr(self, 'microburst_idx'):
+            return
+        # Remove duplicates indicies
+        self.microburst_idx = np.unique(self.microburst_idx)
+        save_path = os.path.join(catalog_save_dir, self.catalog_save_name)
+        print('Saving filtered catalog to {}'.format(save_path))
+        df = self.catalog.iloc[self.microburst_idx]
+        # Remove duplicate times (different than indicies since the same time
+        # from the other sc may be assigned to a different index. 
+        df.drop_duplicates(subset='dateTime')
+        df.to_csv(save_path, index=False)
+        return
+
+    def load_filtered_catalog(self):
+        """
+        Load a filtered catalog and populate the self.microbirst_idx array
+        with existing detections. This method exists to help the user resume
+        the 
+        """
+        filtered_catalog = pd.read_csv(self.catalog_save_path)
+        # Convert the catalog times to datetime objects
+        for timeKey in ['dateTime', 'time_spatial_A', 'time_spatial_B']:
+            filtered_catalog[timeKey] = pd.to_datetime(filtered_catalog[timeKey])
+        # Convert times to numeric for easier comparison
+        flt_times_numeric = date2num(filtered_catalog.dateTime)
+        times_numeric = date2num(self.catalog.dateTime)
+        self.microburst_idx = np.where(np.in1d(times_numeric, flt_times_numeric, 
+                                    assume_unique=True))[0]
+        return
+
+
+callback = Browser(8, width_tol=None, filterDict={})
+# Initialize the GUI
+plt.show()
+# Save the catalog.
+callback.save_filtered_catalog()
---
+++ stats/plot_curtains.py
@@ -0,0 +1,238 @@
+# This program plots the catalog detections that are well-correlated. 
+import os
+import numpy as np
+import pandas as pd
+from datetime import datetime, date, timedelta
+import matplotlib.pyplot as plt
+
+# Paths that are used everywhere in the class
+BASE_DIR = '/home/mike/research/ac6_curtains/'
+CATALOG_DIR = os.path.join(BASE_DIR, 'data/catalogs')
+AC6_DATA_PATH = lambda sc_id: ('/home/mike/research/ac6/ac6{}/'
+                                'ascii/level2'.format(sc_id))
+PLOT_SAVE_DIR = os.path.join(BASE_DIR, 'plots', 'curtain_validation')
+
+class PlotMicrobursts:
+    def __init__(self, catalog_version, plot_width=5, catalog_name=None,
+                plot_save_dir=None, plot_width_flag=False, 
+                make_plt_dir_flag=True, load_sorted_catalog=False):
+        """
+        This class plots the detections from the coincident
+        microburst catalog with a set of default filters.
+        The user can supply other filter values in the 
+        filterDict.
+        """
+        self.plot_width = timedelta(seconds=plot_width)
+        self.catalog_name = catalog_name
+        if plot_save_dir is None:
+            self.plot_save_dir = PLOT_SAVE_DIR
+        else:
+            self.plot_save_dir = plot_save_dir
+
+        if make_plt_dir_flag and (not os.path.exists(self.plot_save_dir)):
+            os.mkdir(self.plot_save_dir)
+            print('Made directory:', self.plot_save_dir)
+        self.plot_width_flag = plot_width_flag
+        self.load_sorted_catalog = load_sorted_catalog
+        self.load_catalog(catalog_version)
+        return
+
+    def filter_catalog(self, defaultFilter=True, filterDict={}):
+        """
+        Apply filters to the catalog file. The default filter
+        applies a filter that should always be used. The dafault
+        filter filters out the events with a temporal_CC < 0.8,
+        detections made outside of 4 < L < 8, filters out detections
+        that have a spatial_CC-0.3 > temporal_CC. Lastly, the default
+        filters removes detections made above the US and SAA to avoid
+        noise from ground transmitters and saturation in the SAA.
+
+        Whenever you want to filter other variables, supply them to
+        the filterDict dictionary. filterDict's keys are the
+        catalog keys and the values is a 2 element list with upper 
+        and lower bounds. If you want a one-sided filter e.g. 
+        Dist_Total greater than 50 km, pass in 
+        filterDict={'Dist_Total':[50, 9999]}.
+        """
+        if defaultFilter:
+            # High CC filter
+            self.catalog = self.catalog[self.catalog['space_cc'] >= 0.8]
+            # Rad belt filter
+            # self.catalog = self.catalog[
+            #                             (np.abs(self.catalog['Lm_OPQ']) > 4) &
+            #                             (np.abs(self.catalog['Lm_OPQ']) < 8)
+            #                             ]
+            # Curtain filter
+            # self.catalog = self.catalog[self.catalog['time_cc'] > 
+            #                             self.catalog['space_cc']+0.3]
+            # USA filter
+            # self.catalog = self.catalog[
+            #         ((self.catalog['lon'] > -60) | (self.catalog['lon'] < -140))|
+            #         ((self.catalog['lat'] > 70) | (self.catalog['lat'] < 15))
+            #                             ]
+            # SAA filter
+            # self.catalog = self.catalog[
+            #         ((self.catalog['lon'] > 30)  | (self.catalog['lon'] < -116)) |
+            #         ((self.catalog['lat'] < -90) | (self.catalog['lat'] > 0))
+            #                     ]
+            # Significane above baseline filter
+            #self.catalog = self.catalog[self.catalog['peak_std'] > 3]
+
+            for key, vals in filterDict.items():
+                self.catalog = self.catalog[
+                    ((self.catalog[key] > min(vals)) & 
+                    (self.catalog[key] < max(vals)))
+                                ]
+        return
+
+    def load_catalog(self, catalog_version):
+        """
+        Load the csv microburst catalog. Any key that has the word time in
+        it is converted to am array of datetime objects. Time keys are:
+        dateTime, time_spatial_A, and time_spatial_B.
+        """
+        # Load a catalog that has been sorted by a person or not.
+        if self.catalog_name is None:
+            if self.load_sorted_catalog: 
+                catalog_name = f'AC6_coincident_microbursts_sorted_v{catalog_version}.txt'
+            else:
+                catalog_name = f'AC6_coincident_microbursts_v{catalog_version}.txt'
+        else:
+            catalog_name = self.catalog_name
+
+        catalog_path = os.path.join(CATALOG_DIR, catalog_name)
+        self.catalog = pd.read_csv(catalog_path)
+        # Convert the catalog times to datetime objects
+        for timeKey in ['dateTime', 'time_spatial_A', 'time_spatial_B']:
+            self.catalog[timeKey] = pd.to_datetime(self.catalog[timeKey])
+        return
+
+    def loop(self, **kwargs):
+        """
+        Loops over the detections that made it through the filters and 
+        make space-time plots.
+        """
+        current_date = date.min
+
+        _, self.ax = plt.subplots(2)
+
+        for _, row in self.catalog.iterrows():
+            if row.dateTime.date() != current_date:
+                # Load current day AC-6 data if not loaded already
+                self.load_ten_hz_data(row.dateTime.date())
+                current_date = row.dateTime.date()
+            self.make_plot(row, **kwargs)
+            self.ax[0].clear()
+            self.ax[1].clear()
+        plt.close()
+        return
+
+    def load_ten_hz_data(self, day):
+        """
+        Load the 10 Hz AC6 data from both spacecraft on date.
+        """
+        time_keys = ['year', 'month', 'day', 'hour', 'minute', 'second']
+        dayStr = '{0:%Y%m%d}'.format(day)
+        pathA = os.path.join(AC6_DATA_PATH('a'), 
+                'AC6-A_{}_L2_10Hz_V03.csv'.format(dayStr))
+        pathB = os.path.join(AC6_DATA_PATH('b'), 
+                'AC6-B_{}_L2_10Hz_V03.csv'.format(dayStr))
+        self.ac6a_data = pd.read_csv(pathA, na_values='-1e+31')
+        self.ac6a_data['dateTime'] = pd.to_datetime(self.ac6a_data[time_keys])
+        self.ac6b_data = pd.read_csv(pathB, na_values='-1e+31')
+        self.ac6b_data['dateTime'] = pd.to_datetime(self.ac6b_data[time_keys])
+        return
+
+    def make_plot(self, row, **kwargs):
+        """
+        This method takes in a dataframe row from the catalog and makes a 
+        space/time plot.
+        """
+        mean_subtracted = kwargs.get('mean_subtracted', True)
+        savefig = kwargs.get('savefig', True)
+        log_scale = kwargs.get('log_scale', False)
+        plot_dos2_and_dos3 = kwargs.get('plot_dos2_and_dos3', True)
+        plot_legend = kwargs.get('plot_legend', True)
+        ax = kwargs.get('ax', self.ax)
+        time_guide_flag = kwargs.get('time_guide_flag', False)
+
+        df_time_a, df_time_b, df_space_a, df_space_b = self._get_filtered_plot_data(row)
+        if mean_subtracted:
+            df_time_a.loc[:, 'dos1rate'] -= df_time_a.loc[:, 'dos1rate'].mean()
+            df_time_b.loc[:, 'dos1rate'] -= df_time_b.loc[:, 'dos1rate'].mean()
+            df_space_a.loc[:, 'dos1rate'] -= df_space_a.loc[:, 'dos1rate'].mean()
+            df_space_b.loc[:, 'dos1rate'] -= df_space_b.loc[:, 'dos1rate'].mean()
+
+            if plot_dos2_and_dos3:
+                df_time_a.loc[:, 'dos2rate'] -= df_time_a.loc[:, 'dos2rate'].mean()
+                df_time_b.loc[:, 'dos2rate'] -= df_time_b.loc[:, 'dos2rate'].mean()
+                #df_space_a.loc[:, 'dos2rate'] -= df_space_a.loc[:, 'dos2rate'].mean()
+                df_time_a.loc[:, 'dos3rate'] -= df_time_a.loc[:, 'dos3rate'].mean()
+                #df_space_a.loc[:, 'dos3rate'] -= df_space_a.loc[:, 'dos3rate'].mean()
+                
+        ax[0].plot(df_time_a['dateTime'], df_time_a['dos1rate'], 'r', label='AC6-A dos1')
+        if plot_dos2_and_dos3:
+            ax[0].plot(df_time_a['dateTime'], df_time_a['dos2rate'], 'r:', label='AC6-A dos2')
+            ax[0].plot(df_time_a['dateTime'], df_time_a['dos3rate'], 'r--', label='AC6-A dos3')
+            
+        ax[0].plot(df_time_b['dateTime'], df_time_b['dos1rate'], 'b', label='AC6-B')
+        if plot_dos2_and_dos3:
+            ax[0].plot(df_time_b['dateTime'], df_time_b['dos2rate'], 'b:', label='AC6-B dos2')
+        if time_guide_flag:
+            ax[0].axvline(row.at['dateTime'])
+        if plot_legend:
+            ax[0].legend(loc=1)
+        ax[1].plot(df_space_a['dateTime'], df_space_a['dos1rate'], 'r', label='AC6-A')
+        ax[1].plot(df_space_b['dateTime'], df_space_b['dos1rate'], 'b', label='AC6-B')
+
+        if log_scale:
+            ax[0].set_yscale('log')
+            ax[1].set_yscale('log')
+
+        # Print peak width if it exists in the catalog.
+        if set(['peak_width_A', 'peak_width_B']).issubset(row.index) and self.plot_width_flag:
+            s = 'peak_width_A = {} s\npeak_width_B = {} s'.format(
+                    round(row['peak_width_A'], 2), round(row['peak_width_B'], 2))
+            ax[0].text(0, 1, s, transform=ax[0].transAxes, va='top')
+            
+        # Print the time shift seconds
+        ax[0].text(0, 1, f'shifted by = {row.Lag_In_Track} s', transform=ax[0].transAxes, va='top')
+
+        if savefig:
+            save_name = '{0:%Y%m%d_%H%M%S}_ac6_curtain_validation.png'.format(
+                        row['dateTime'])
+            plt.savefig(os.path.join(self.plot_save_dir, save_name))
+        return
+
+    def _get_filtered_plot_data(self, row):
+        df_time_a = self.ac6a_data[
+                            (self.ac6a_data['dateTime'] > row['dateTime']-self.plot_width/2) & 
+                            (self.ac6a_data['dateTime'] < row['dateTime']+self.plot_width/2)
+                            ]
+        df_time_b = self.ac6b_data[
+                            (self.ac6b_data['dateTime'] > row['dateTime']-self.plot_width/2) & 
+                            (self.ac6b_data['dateTime'] < row['dateTime']+self.plot_width/2)
+                            ]
+        if row.at['dateTime'] == row.at['time_spatial_A']:
+            df_space_a = df_time_a
+            df_space_b = self.ac6b_data[
+                            (self.ac6b_data['dateTime'] > row['time_spatial_B']-self.plot_width/2) & 
+                            (self.ac6b_data['dateTime'] < row['time_spatial_B']+self.plot_width/2)
+                            ]
+            df_space_b.loc[:, 'dateTime'] -= timedelta(seconds=row.at['Lag_In_Track'])
+        elif row.at['dateTime'] == row.at['time_spatial_B']:
+            df_space_a = self.ac6a_data[
+                            (self.ac6a_data['dateTime'] > row['time_spatial_A']-self.plot_width/2) & 
+                            (self.ac6a_data['dateTime'] < row['time_spatial_A']+self.plot_width/2)
+                            ]
+            df_space_a.loc[:, 'dateTime'] += timedelta(seconds=row.at['Lag_In_Track'])
+            df_space_b = df_time_b
+        else:
+            raise(ValueError('No space matches found!'))
+        return df_time_a, df_time_b, df_space_a, df_space_b
+
+if __name__ == '__main__':
+    version = 8
+    p = PlotMicrobursts(version, catalog_name=f'AC6_coincident_microbursts_v{version}.txt')
+    p.filter_catalog(filterDict={})
+    p.loop(mean_subtracted=False)
