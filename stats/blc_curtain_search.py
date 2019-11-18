5dc73893b0d1c834120816fe469d3da246a15b0d
mike <msshumko@gmail.com>
2019-11-18 12:03:40 -0700

    Added the microburst dataset for comparison and added l_mlt_microburst_dist as well.
    blc_curtain_search.py - looks for curtains in the BLC. None
    were found.

---
+++ stats/blc_curtain_search.py
@@ -0,0 +1,23 @@
+# Look for curtains in the BLC
+import numpy as np
+import pandas as pd
+import os
+
+BASE_DIR = '/home/mike/research/ac6_curtains/'
+CATALOG_NAME = 'AC6_curtains_sorted_v8.txt'
+CATALOG_PATH = os.path.join(BASE_DIR, 'data/catalogs', CATALOG_NAME)
+cat = pd.read_csv(CATALOG_PATH)
+
+def filter_blc(cat, lat_edges=[30, 75], lon_edges=[30, -45]):
+    """ 
+    Takes in a catalog and filters out events outside of 
+    the lat_edges and lon_edges. 
+    """
+    cat = cat[(cat.loc[:, 'lat'] > lat_edges[0]) & 
+              (cat.loc[:, 'lat'] < lat_edges[1])]
+    cat = cat[(cat.loc[:, 'lon'] > lon_edges[0]) & 
+              (cat.loc[:, 'lon'] < lon_edges[1])]
+    return cat
+
+print(f'Total number of curtains = {cat.shape[0]}')
+print(f'Number of curtains in the BLC = {filter_blc(cat).shape[0]}')
--- stats/l_mlt_microburst_dist.py
+++ stats/l_mlt_microburst_dist.py
@@ -11,7 +11,7 @@ BASE_DIR = '/home/mike/research/ac6_curtains/'
 CATALOG_NAME = 'AC6_curtains_sorted_v8.txt'
 CATALOG_PATH = os.path.join(BASE_DIR, 'data/catalogs', CATALOG_NAME)
 cat = pd.read_csv(CATALOG_PATH)
-NORM_FLAG = False
+NORM_FLAG = True
 
 # Load the L-MLT normalization files.
 with open('/home/mike/research/ac6_curtains/data/norm/ac6_L_MLT_bins_same_loc.csv') as f:
@@ -36,7 +36,7 @@ def draw_earth(ax, earth_resolution=50):
     ax.fill_between(*earth_shadow, color='k')
     return
 
-def draw_L_contours(ax, L_labels=[2, 4, 6, 8, 10], earth_resolution=50):
+def draw_L_contours(ax, L_labels=[2, 4, 6, 8], earth_resolution=50):
     """ Plots a subset of the L shell contours. """
     # Draw azimuthal lines for a subset of L shells.
     L_labels_names = [str(i) for i in L_labels[:-1]] + [f'L = {L_labels[-1]}']
@@ -60,10 +60,7 @@ if NORM_FLAG:
     cat_dist *= scaling_factors
 
 mltmlt, ll = np.meshgrid(mlt_bins, lm_bins)
-if NORM_FLAG:
-    p1 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap='Reds', vmax=300)
-else:
-    p1 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap='Reds')
+p1 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap='Reds')
 plt.colorbar(p1, ax=ax[0], label=r'Number of curtains', pad=0.1)
 
 # L shell filter for the L-MLT plot
@@ -86,7 +83,7 @@ L_labels, L_labels_names = draw_L_contours(ax[1])
 ax[0].set_xlabel('MLT')
 ax[0].set_title(f'(a) AC6 curtain distribution\nNormalized = {NORM_FLAG}', y=1.08)
 ax[1].set_xlabel('MLT')
-ax[1].set_title('(b) AC6 distribution of 10 Hz data\nsame location', y=1.08)
+ax[1].set_title('(b) AC6 simultaneous data avaliability', y=1.08)
 ax[0].set_theta_zero_location("S") # Midnight at bottom
 ax[1].set_theta_zero_location("S") # Midnight at bottom
 mlt_labels = (ax[1].get_xticks()*12/np.pi).astype(int)
