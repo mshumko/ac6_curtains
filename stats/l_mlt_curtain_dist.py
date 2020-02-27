8cd036f6438a8cdd33e00bb28044abad2ba7094e
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-02-27 10:01:53 -0700

    Small changes to L-MLT dist and started an updated presentation.

--- stats/l_mlt_curtain_dist.py
+++ stats/l_mlt_curtain_dist.py
@@ -9,7 +9,7 @@ plt.rcParams.update({'font.size':13})
 
 import dirs
 
-CATALOG_NAME = 'AC6_curtains_sorted_v8.txt'
+CATALOG_NAME = 'AC6_curtains_sorted_vNone.txt'
 CATALOG_PATH = os.path.join(dirs.CATALOG_DIR, CATALOG_NAME)
 cat = pd.read_csv(CATALOG_PATH)
 NORM_FLAG = False
@@ -49,57 +49,64 @@ def draw_L_contours(ax, L_labels=[2, 4, 6, 8], earth_resolution=50):
     return L_labels, L_labels_names
 
 
-fig = plt.figure(figsize=(11, 5))
-ax = 2*[None]
-ax[0] = plt.subplot(121, projection='polar')
-ax[1] = plt.subplot(122, projection='polar')
+fig = plt.figure(figsize=(14, 5))
+ax = 3*[None]
+ax[0] = plt.subplot(131, projection='polar')
+ax[1] = plt.subplot(132, projection='polar')
+ax[2] = plt.subplot(133, projection='polar')
 
 cat_dist, mlt_bins, lm_bins = np.histogram2d(cat.MLT_OPQ, cat.Lm_OPQ,
                      bins=[bins['MLT_OPQ'], bins['Lm_OPQ']])
-if NORM_FLAG:
-    scaling_factors = (np.max(norm)/norm).T
-    # Set the sectors with no observations or little observations to NaN.
-    scaling_factors[np.isinf(scaling_factors)] = np.nan
-    scaling_factors[norm.T < 20000] = np.nan
-    cat_dist *= scaling_factors
+
+scaling_factors = (np.max(norm)/norm).T
+# Set the sectors with no observations or little observations to NaN.
+scaling_factors[np.isinf(scaling_factors)] = np.nan
+scaling_factors[norm.T < 20000] = np.nan
+cat_norm = cat_dist*scaling_factors
 
 #cat_dist = np.ma.masked_invalid(scaling_factors)
 
 mltmlt, ll = np.meshgrid(mlt_bins, lm_bins)
-if NORM_FLAG:
-    p1 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap=COLOR_MAP, vmax=250)
-else:
-    p1 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap=COLOR_MAP)
-plt.colorbar(p1, ax=ax[0], label=r'Number of curtains', pad=0.1)
+
+p0 = ax[0].pcolormesh(mltmlt*np.pi/12, ll, cat_dist.T, cmap=COLOR_MAP)
+p1 = ax[1].pcolormesh(mltmlt*np.pi/12, ll, cat_norm.T, cmap=COLOR_MAP, vmax=400)
+plt.colorbar(p0, ax=ax[0], label=r'True Number of curtains', pad=0.1, orientation='horizontal')
+plt.colorbar(p1, ax=ax[1], label=r'Normalized Number of curtains', pad=0.1, orientation='horizontal')
 
 # L shell filter for the L-MLT plot
 L_lower = 0
 idL = np.where(np.array(bins['Lm_OPQ']) >= L_lower)[0][0]
-p2 = ax[1].pcolormesh(np.array(bins['MLT_OPQ'])*np.pi/12, 
+p2 = ax[2].pcolormesh(np.array(bins['MLT_OPQ'])*np.pi/12, 
                     bins['Lm_OPQ'], norm/1E5, 
                     cmap=COLOR_MAP, vmax=3)
-plt.colorbar(p2, ax=ax[1], label=r'10 Hz Samples x $10^5$', pad=0.1)
+plt.colorbar(p2, ax=ax[2], label=r'10 Hz Samples x $10^5$', pad=0.1, orientation='horizontal')
 
 # Draw Earth and shadow
 draw_earth(ax[0])
 draw_earth(ax[1])
+draw_earth(ax[2])
 
 # Draw L shell contours
 draw_L_contours(ax[0])
-L_labels, L_labels_names = draw_L_contours(ax[1])
+draw_L_contours(ax[1])
+L_labels, L_labels_names = draw_L_contours(ax[2])
+L_label_colors = ['w', 'k', 'w']
 
 ### PLOT TWEEKS ###
-ax[0].set_title(f'(a) AC6 curtain distribution\nNormalized = {NORM_FLAG}', y=1.08)
-ax[1].set_title('(b) AC6 distribution of 10 Hz data\nsame location', y=1.08)
+ax[0].set_title(f'(a) AC6 curtain distribution', y=1.08)
+ax[1].set_title(f'(b) Normalized AC6 curtain distribution', y=1.08)
+ax[2].set_title('(c) AC6 distribution of 10 Hz data', y=1.08)
 mlt_labels = (ax[1].get_xticks()*12/np.pi).astype(int)
 
-for a in ax:
-    a.set_xlabel('MLT')
+
+for i, a in enumerate(ax):
+    a.set_xlabel('MLT', labelpad=-2)
     a.set_theta_zero_location("S") # Midnight at bottom
     a.set_xticklabels(mlt_labels) # Transform back from 0->2pi to 0->24.
     a.set_yticks(L_labels)
-    a.set_yticklabels(L_labels_names)
+    a.set_yticklabels(L_labels_names, color=L_label_colors[i])
     a.set_ylim(top=10)
     
-plt.tight_layout(rect=(0.02, 0.05, 1, 0.9))
+#plt.tight_layout(rect=(0.02, 0.05, 1, 0.9))
+plt.tight_layout()
 plt.show()
