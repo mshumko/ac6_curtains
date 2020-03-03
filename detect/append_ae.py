cd5b08076a5d0d41c456e8459dc181e3db277b7c
mike <msshumko@gmail.com>
2020-03-03 11:07:51 -0700

    append_ae.py - Finished writing the code to append the AE index to the curtain catalog. Pandas works incredibly quickly!

--- detect/append_ae.py
+++ detect/append_ae.py
@@ -6,7 +6,7 @@ import glob
 import dirs
 
 class Append_AE:
-    def __init__(self, catalog_name, ae_dir=None):
+    def __init__(self, catalog_name, ae_dir=None, save_name=None):
         """
         This class uses AE data downloaded from 
         http://wdc.kugi.kyoto-u.ac.jp/aeasy/index.html
@@ -15,6 +15,7 @@ class Append_AE:
         minute of the curtain observation.
         """
         self.catalog_name = catalog_name
+        self.cat_path = os.path.join(dirs.CATALOG_DIR, self.catalog_name)
         self.load_catalog()
 
         if ae_dir is None:
@@ -22,11 +23,12 @@ class Append_AE:
         else:
             self.ae_dir = ae_dir
         self.load_ae()
+        self.merge()
+        self.save_catalog(save_name=save_name)
         return
 
     def load_catalog(self):
-        cat_path = os.path.join(dirs.CATALOG_DIR, self.catalog_name)
-        self.cat = pd.read_csv(cat_path, index_col=0)
+        self.cat = pd.read_csv(self.cat_path, index_col=0)
         self.cat.index = pd.to_datetime(self.cat.index)
         return
 
@@ -46,13 +48,24 @@ class Append_AE:
             year_ae.index=year_ae.DATE_TIME
             del year_ae['DATE_TIME']
             self.ae = self.ae.append(year_ae)
+        
+        # Drop duplicate indicies
+        self.ae = self.ae.groupby(self.ae.index).first()
         return
 
     def merge(self, tolerance_min=1):
 
-        pd.merge_asof(self.cat, self.ae, left_index=True, 
+        self.cat = pd.merge_asof(self.cat, self.ae, left_index=True, 
                     right_index=True, direction='nearest', 
                     tolerance=pd.Timedelta(minutes=tolerance_min))
+        return
+
+    def save_catalog(self, save_name=None):
+        """
+        Saves the edited catalog to save_name which is the same as 
+        self.catalog_name by default.
+        """
+        self.cat.to_csv(self.cat_path, index=False)
         return
 
 if __name__ == '__main__':
