dcb2d57e2dd0635661b51376313eddab422ce8c2
Mykhaylo Mike Shumko <msshumko@gmail.com>
2020-03-04 12:07:11 -0700

    Finished writing the loop method to calculate peak widths. Now need to put the plot test code into its own method.

--- detect/calculate_curtain_width.py
+++ detect/calculate_curtain_width.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import dirs
 import detect_daily
 
-class Curtain_Width:
+class Curtain_Width(detect_daily.DetectDailyCurtains):
     def __init__(self, catalog_name, rel_height=0.5):
         """
 
@@ -15,6 +15,8 @@ class Curtain_Width:
         self.catalog_name = catalog_name
         self.catalog_path = os.path.join(dirs.CATALOG_DIR, self.catalog_name)
         self.rel_height = rel_height
+
+        super().__init__(None)
 
         self.load_catalog()
         return
@@ -33,31 +35,67 @@ class Curtain_Width:
         return
 
     def loop(self):
+        """
+        Loop over all of the days and find the peak width for each curtain.
+        """
         width_df = pd.DataFrame(data=np.nan*np.ones((self.cat.shape[0], 2)),
                                 index=self.cat.index,
                                 columns=['width_A', 'width_B'])
-        print(width_df)
         current_date = pd.to_datetime('2014-01-01')
 
-        for i, (date, row) in enumerate(self.cat.iterrows()):
-            # date = date.date()
-
+        for i, (t0, row) in enumerate(self.cat.iterrows()):
             # Load the data from this day if it has not already.
-            if current_date != date.date():
-                daily_detections = detect_daily.DetectDailyCurtains(date)
-                daily_detections.load_data(date)
-                daily_detections.shift_time()
-                daily_detections.align_space_time_stamps()
-                current_date = date
+            if current_date != t0.date():
+                #daily_detections = detect_daily.DetectDailyCurtains(t0)
+                self.date = t0.date()
+                self.load_data(t0)
+                self.shift_time()
+                self.align_space_time_stamps()
+                current_date = t0
+
+            center_time_A = np.where(self.df_a['dateTime'] == t0)[0]
+            center_time_B = np.where(self.df_b['dateTime_shifted'] == t0)[0]
+            assert ((len(center_time_A) == 1) and (len(center_time_B) == 1)), 'No matches found!'
+            peak_id_window_dp = 10    
+            peak_A = np.argmax(self.df_a['dos1rate'][center_time_A[0]-peak_id_window_dp:center_time_A[0]+peak_id_window_dp])
+            peak_B = np.argmax(self.df_b['dos1rate'][center_time_B[0]-peak_id_window_dp:center_time_B[0]+peak_id_window_dp])    
+
+            widths_A, widths_B = self.calc_peak_width(peak_A, peak_B)
+
+            plot_width = 10
+            plot_width_dp = plot_width//2*10
+            if plot_width:
+                plt.plot(self.df_a['dateTime'][center_time_A[0]-plot_width_dp:center_time_A[0]+plot_width_dp], 
+                        self.df_a['dos1rate'][center_time_A[0]-plot_width_dp:center_time_A[0]+plot_width_dp], 'r')
+                plt.plot(self.df_b['dateTime_shifted'][center_time_B[0]-plot_width_dp:center_time_B[0]+plot_width_dp], 
+                        self.df_b['dos1rate'][center_time_B[0]-plot_width_dp:center_time_B[0]+plot_width_dp], 'b')
+                plt.axvline(self.df_a['dateTime'][peak_A], c='r')
+                plt.axvline(self.df_b['dateTime_shifted'][peak_B], c='b')
+
+                plt.hlines(widths_A[1], 
+                            self.df_a['dateTime'][int(widths_A[2])], 
+                            self.df_a['dateTime'][int(widths_A[3])], 
+                            color="r", lw=3)
+                plt.hlines(widths_B[1], 
+                            self.df_b['dateTime_shifted'][int(widths_B[2])], 
+                            self.df_b['dateTime_shifted'][int(widths_B[3])], 
+                            color="b", lw=3)
+                # plt.hlines(*widths_B[1:], color="b")
+
+                plt.title(row.Lag_In_Track)
+                plt.show()
+            print(center_time_A, peak_A, center_time_B, peak_B)
+            #print(self.calc_peak_width(peak_A, peak_B))
 
         return
 
-    def calc_peak_width(self):
+    def calc_peak_width(self, peak_A, peak_B):
         """
 
         """
-        widths = scipy.signal.peak_widths(x, peaks, rel_height=self.rel_height)
-        return
+        widths_A = scipy.signal.peak_widths(self.df_a['dos1rate'], [peak_A], rel_height=self.rel_height)
+        widths_B = scipy.signal.peak_widths(self.df_b['dos1rate'], [peak_B], rel_height=self.rel_height)
+        return widths_A, widths_B
 
 if __name__ == '__main__':
     c = Curtain_Width('AC6_curtains_baseline_method_sorted_v0.txt')
