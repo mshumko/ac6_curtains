254275a60ffd14c109ea80bca8936f1520621be5
mike <msshumko@gmail.com>
2020-04-21 15:07:17 -0600

    Added standard error error bars to curtain width distribution.

--- stats/curtain_width_stats.py
+++ stats/curtain_width_stats.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os 
+import scipy.stats
 
 import dirs
 
@@ -27,17 +28,39 @@ coincident_microburst_cat = pd.read_csv(coincident_microburst_catalog_path)
 
 # Calculate the curtain probability density
 size_bin_width_km = 5
-max_bin = 56
-size_bins = np.arange(0, max_bin, size_bin_width_km)
+max_bin_km = 56
+size_bins = np.arange(0, max_bin_km, size_bin_width_km)
 # Filter similar curtain widths
 idx = (
       (curtain_cat['width_B']/curtain_cat['width_A'] < 1+similar_thresh) &
       (curtain_cat['width_A']/curtain_cat['width_B'] < 1+similar_thresh)
       )
 curtain_cat = curtain_cat[idx]
-# Histogram this
-H_size, _ = np.histogram(7.5*curtain_cat['width_B'], 
-                        density=True, bins=size_bins)
+
+def mc_err(mu, n_iter=1000):
+      """ 
+      Calculate the Monte Carlo standard error of n, an array of 
+      counts, assuming poisson statistics 
+      """
+      err = np.nan*np.zeros_like(mu)
+
+      for i, mu_i in enumerate(mu):
+            dist = scipy.stats.poisson(mu_i).rvs(size=n_iter)
+            #q = dist.std()
+            #err[i] = (q.iloc[1] - q.iloc[0])/2
+            err[i] = np.std(dist)
+      return err
+
+# Histogram and estimate errors for curtains
+curtain_hist, _ = np.histogram(7.5*curtain_cat['width_B'], 
+                        bins=size_bins)
+curtain_mc_std = mc_err(curtain_hist)
+curtain_fraction_mid = curtain_hist/(curtain_cat.shape[0]*size_bin_width_km)
+curtain_fraction_upper = (curtain_hist+curtain_mc_std)/\
+      (size_bin_width_km*(sum(curtain_hist)+sum(curtain_mc_std)))
+curtain_fraction_lower = (curtain_hist-curtain_mc_std)/\
+      (size_bin_width_km*(sum(curtain_hist)-sum(curtain_mc_std)))
+curtain_fraction_err = np.abs(curtain_fraction_upper-curtain_fraction_lower)
 
 # Calculate the microburst probability density.
 all_microbursts, _ = np.histogram(microburst_cat['Dist_In_Track'], 
@@ -52,10 +75,16 @@ print(f'{100*(sum(curtain_cat["width_A"] < max_size/7.5))/len(curtain_cat["width
       f'of curtains are less than {max_size} km wide.')
 
 fig, ax = plt.subplots()
-ax.step(size_bins, np.append(H_size, np.nan), where='post', c='k', 
-      label='curtain', lw=4)
+ax.step(size_bins, np.append(curtain_fraction_mid, np.nan), where='post', c='k', 
+      label='curtain', lw=2)
+ax.errorbar(size_bins+size_bin_width_km/2, np.append(curtain_fraction_mid, np.nan), 
+            yerr=np.append(curtain_fraction_err, np.nan),
+            ls='', color='k', capsize=3, lw=2)
+
 ax.step(size_bins, np.append(microburst_fraction, np.nan), c='r', 
       where='post', label='microburst', lw=2)
+# ax.errorbar(size_bins+size_bin_width_km/2, np.append(microburst_fraction, np.nan), 
+#             yerr=np.append(microburst_fraction_err, np.nan), ls='', color='r', capsize=2, lw=1)
 
 ax.set(title='Distribution of > 30 keV curtain and microburst sizes',
             ylabel='Probability density', xlim=(0, size_bins[-1]), 
