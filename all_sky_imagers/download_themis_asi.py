8c4d1888fff6af8aa759dbba84609755fd58d779
mike <msshumko@gmail.com>
2020-05-15 18:27:14 -0600

    Writing the script to download the THEMIS ASI movies. Will need to download the calibration files as well.

---
+++ all_sky_imagers/download_themis_asi.py
@@ -0,0 +1,45 @@
+# This script downloads the THEMIS asi data using the themisasi
+# API.
+
+import numpy as np
+import pandas as pd
+import pathlib
+import urllib.request
+from datetime import datetime
+
+import themisasi
+import themisasi.web
+
+from ac6_curtains import dirs
+
+def load_curtain_catalog(catalog_name):
+    cat_path = dirs.CATALOG_DIR / catalog_name
+    cat = pd.read_csv(cat_path, index_col=0, parse_dates=True)
+    return cat
+
+def download_asi(time, station):
+    base_url = 'http://themis.ssl.berkeley.edu/data/themis/thg/l1/asi/'
+    station_url = (f'{station.lower()}/{time.year}/{time.strftime("%m")}/')
+    file_name = f'thg_l1_asf_{station.lower()}_{time.strftime("%Y%m%d%H")}_v01.cdf'
+
+    try:
+        urllib.request.urlretrieve(base_url + station_url + file_name, 
+                                dirs.ASI_DIR / file_name)
+    except urllib.error.HTTPError as err:
+        if '404' in str(err):
+            print(base_url + station_url + file_name)
+            raise
+        else:
+            raise
+
+    return
+
+if __name__ == '__main__':
+    catalog_name = 'AC6_curtains_themis_asi_5deg.csv'
+    cat = load_curtain_catalog(catalog_name)
+    # url = 'http://themis.ssl.berkeley.edu/data/themis/thg/l1/asi/kuuj/2017/04/thg_l1_asf_kuuj_2017041705_v01.cdf'
+    # # urllib.request.urlretrieve(url, 'test.cdf')
+    # # print(cat)
+    # # themisasi.web.download(['2015-04-04T06'], ['SNAP'], pathlib.Path(dirs.BASE_DIR, 'data'), ['http://themis.ssl.berkeley.edu/data/themis/thg/l1/asi/'])
+
+    download_asi(datetime(2017, 4, 1, 4), 'kuuj')
--- dirs.py
+++ dirs.py
@@ -1,10 +1,11 @@
 import pathlib
 
 # Specify AC6 directories
-BASE_DIR = '/home/mike/research/ac6_curtains/'
+BASE_DIR = pathlib.Path('/home/mike/research/ac6_curtains/')
 CATALOG_DIR = pathlib.Path(BASE_DIR, 'data/catalogs')
 NORM_DIR = pathlib.Path(BASE_DIR, 'data/norm')
 AC6_DATA_PATH = lambda sc_id: (f'/home/mike/research/ac6/ac6{sc_id}/'
                                 'ascii/level2')
 AC6_MERGED_DATA_DIR = '/home/mike/research/ac6/merged/'
 PLOT_SAVE_DIR = pathlib.Path(BASE_DIR, 'plots', 'curtain_validation')
+ASI_DIR = BASE_DIR / 'data' / 'asi'
