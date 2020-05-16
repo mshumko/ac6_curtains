8b95e5a364e01a8dfcafa278d592a0993e271989
mike <msshumko@gmail.com>
2020-05-15 18:52:49 -0600

    Added the downloader for the THEMIS calibration data.

--- all_sky_imagers/download_themis_asi.py
+++ all_sky_imagers/download_themis_asi.py
@@ -6,6 +6,8 @@ import pandas as pd
 import pathlib
 import urllib.request
 from datetime import datetime
+from bs4 import BeautifulSoup
+import re
 
 import themisasi
 import themisasi.web
@@ -17,7 +19,25 @@ def load_curtain_catalog(catalog_name):
     cat = pd.read_csv(cat_path, index_col=0, parse_dates=True)
     return cat
 
-def download_asi(time, station):
+def download_asi_calibration(station):
+    base_url = 'http://themis.ssl.berkeley.edu/data/themis/thg/l2/asi/cal/'
+    
+    # Find all cdf files with the station name
+    html = urllib.request.urlopen(base_url).read().decode('utf-8')
+    # Scrape the HTML
+    soup = BeautifulSoup(html, 'html.parser')
+    # Extract all cdf files
+    file_name_html = soup.findAll(href=re.compile("\.cdf$"))
+    # Extract all hyperlinks (href) filenames with the station name.
+    file_names = [file_name.get('href') for file_name in file_name_html 
+                    if station.lower() in file_name.get('href')]
+
+    for file_name in file_names:
+        urllib.request.urlretrieve(base_url + file_name, 
+                                dirs.ASI_DIR / file_name)
+    return
+
+def download_asi_movie(time, station):
     base_url = 'http://themis.ssl.berkeley.edu/data/themis/thg/l1/asi/'
     station_url = (f'{station.lower()}/{time.year}/{time.strftime("%m")}/')
     file_name = f'thg_l1_asf_{station.lower()}_{time.strftime("%Y%m%d%H")}_v01.cdf'
@@ -31,7 +51,6 @@ def download_asi(time, station):
             raise
         else:
             raise
-
     return
 
 if __name__ == '__main__':
@@ -42,4 +61,4 @@ if __name__ == '__main__':
     # # print(cat)
     # # themisasi.web.download(['2015-04-04T06'], ['SNAP'], pathlib.Path(dirs.BASE_DIR, 'data'), ['http://themis.ssl.berkeley.edu/data/themis/thg/l1/asi/'])
 
-    download_asi(datetime(2017, 4, 1, 4), 'kuuj')
+    # download_asi(datetime(2017, 4, 1, 4), 'kuuj')
